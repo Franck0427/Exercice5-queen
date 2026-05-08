318990ce17459824bcf3fe18e20bb9ecabec0ec7
I3R2T1P26 <prunekabran@gmail.com>
2026-05-08 19:00:33 +0000

    commit initialisation Reine

---
+++ src/Queen.java
@@ -0,0 +1,25 @@
+class Queen extends Piece {
+
+    @Override
+    boolean isValidMove(Position newPosition) {
+        // 1. On vérifie d'abord si la position est sur le plateau (via la classe Piece)
+        if (!super.isValidMove(newPosition)) {
+            return false;
+        }
+
+        // 2. Logique de la Reine : Mouvement de Tour OU mouvement de Fou
+
+        // Condition de la Tour (Rock)
+        boolean isRockMove = (newPosition.column == this.position.column || newPosition.row == this.position.row);
+
+        // Condition du Fou (Bishop)
+        boolean isBishopMove = (Math.abs(newPosition.column - this.position.column) == Math.abs(newPosition.row - this.position.row));
+
+        // La reine est valide si l'une des deux conditions est vraie
+        if (isRockMove || isBishopMove) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
